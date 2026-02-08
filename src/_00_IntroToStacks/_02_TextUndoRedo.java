58dee9c1110598850a20aeb118285e79ddbd7634
league <league@192.168.1.78>
2026-02-07 16:29:29 -0800

    lots of progress on TextUndoRedo

--- src/_00_IntroToStacks/_02_TextUndoRedo.java
+++ src/_00_IntroToStacks/_02_TextUndoRedo.java
@@ -5,6 +5,7 @@ import javax.swing.JLabel;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Stack;
 public class _02_TextUndoRedo implements KeyListener {
     /* 
      * Create a JFrame with a JPanel and a JLabel.
@@ -21,15 +22,41 @@ public class _02_TextUndoRedo implements KeyListener {
      * pressed, the top Character is popped  off the Stack and added back to
      * the JLabel.
      */
-
+public static void main(String[] args) {
+	_02_TextUndoRedo bob = new _02_TextUndoRedo();
+	bob.run();
+}
 JFrame frame = new JFrame();
 JLabel label = new JLabel();
+String jeff = "";
+Stack<Character> letters = new Stack<Character>();
+Stack<Character> removed = new Stack<Character>();
 void run () {
+	frame.setVisible(true);
 	frame.addKeyListener(this);
+	frame.add(label);
+	frame.pack();
 }
 @Override
 public void keyPressed(KeyEvent arg0) {
 	// TODO Auto-generated method stub
+	int pressed = arg0.getKeyCode();
+	char pressedChar = arg0.getKeyChar();
+	System.out.println(pressed);
+	System.out.println(pressedChar);
+	letters.push(pressedChar);
+	String bob = ""+letters;
+	label.setText(bob);
+	if(pressed == 8) {
+		char popped = letters.pop();
+		removed.push(popped);
+		label.setText(""+letters);
+	}
+	if(pressed ==48) {
+		char popback = removed.pop();
+		letters.push(popback);
+		label.setText(""+letters);
+	}
 	
 }
 @Override
